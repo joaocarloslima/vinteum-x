bf59b7a836c4e9559ad44de9fda87723fc4248aa
joaocarloslima <joaocarloslima@me.com>
2026-02-11 15:02:17 -0300

    fix cartas to final

--- src/main/java/com/example/Jogador.java
+++ src/main/java/com/example/Jogador.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Jogador {
 
     private int pontos = 0;
-    private List<Carta> cartas = new ArrayList<>();
+    private final List<Carta> cartas = new ArrayList<>();
     private boolean parou = false;
 
     public void receberCarta(Carta carta){
